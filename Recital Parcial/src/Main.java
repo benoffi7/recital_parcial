b385d408203ad8de39c5447edddf7c3756f3419c
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-05-09 14:09:56 -0300

    Eliminado de comentarios del IDE

--- Recital Parcial/src/Main.java
+++ Recital Parcial/src/Main.java
@@ -5,16 +5,11 @@ import modelo.entradas.EntradaGeneral;
 import modelo.entradas.EntradaVIP;
 import modelo.interfaces.IControlPublico;
 import modelo.interfaces.IVentaEntradas;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+
         EntradaGeneral entrada = new EntradaGeneral(1,"codo");
         entrada.obtenerPrecioTotal();
 
