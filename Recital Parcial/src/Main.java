5d24cb5caa80ee37bd3dd36d41137387833a7509
Matias Antonelli <matiasantonelli6874@gmail.com>
2024-05-09 14:12:40 -0300

    Eliminado de clases fuera del parcial

--- Recital Parcial/src/Main.java
+++ Recital Parcial/src/Main.java
@@ -1,12 +1,7 @@
-import modelo.PartidoFutbol;
-import modelo.Recital;
 import modelo.entradas.Entrada;
 import modelo.entradas.EntradaGeneral;
 import modelo.entradas.EntradaVIP;
-import modelo.interfaces.IControlPublico;
-import modelo.interfaces.IVentaEntradas;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,38 +12,8 @@ public class Main {
         entradaVIP.obtenerPrecioTotal();
 
 
-        ArrayList<IVentaEntradas> iVentaEntradas = new ArrayList<>();
-        iVentaEntradas.add(new Recital());
-        iVentaEntradas.add(new PartidoFutbol());
-
-        venderEntradas(new Recital());
-
-        venderEntradas(new PartidoFutbol());
-        controlPublico(new PartidoFutbol());
-
-       // getEntrada(new EntradaGeneral());
-       // getEntrada(new EntradaVIP());
-
-        System.out.println("Hola!");
-
     }
 
-    public static void venderEntradas (IVentaEntradas iVentaEntradas)
-    {
-    //soy un cambio de la rama
-    }
-
-    public static void controlPublico (IControlPublico iControlPublico)
-    {
-
-    }
-
-
-
-    public static void getEntrada(Entrada entrada)
-    {
-
-    }
 
 
 }
